64b8d6b4f2511a6dcf7f08a2778b1733e011b377
Ricardo <r.goncalo@campus.fct.unl.pt>
2022-09-15 17:51:24 +0100

    Nova funcionalidade de varios numeros e emails num unico contacto

--- src/contactBook/Contact.java
+++ src/contactBook/Contact.java
@@ -1,14 +1,23 @@
 package contactBook;
 
+import java.util.HashMap;
+
 public class Contact {
     private String name;
     private int phone;
+
+    private HashMap<String, Integer> additionalPhones;
+
     private String email;
+
+    private HashMap<String, String> additionalEmails;
 
     public Contact(String name, int phone, String email) {
         this.name = name;
         this.phone = phone;
         this.email = email;
+        additionalEmails = new HashMap<String, String>();
+        additionalPhones = new HashMap<String, Integer>();
     }
 
     public String getName() {
@@ -26,6 +35,14 @@ public class Contact {
     public void setPhone(int phone) {
         this.phone = phone;
     }
+
+    public void addOtherEmail(String key, String email){ additionalEmails.put(key, email); }
+
+    public void addOtherNumber(String key, int number){ additionalPhones.put(key, number); }
+
+    public void remOtherEmail(String key){ additionalEmails.remove(key);}
+
+    public void remOtherNumber(String key) {additionalPhones.remove(key);}
 
     public void setEmail(String email) {
         this.email = email;
