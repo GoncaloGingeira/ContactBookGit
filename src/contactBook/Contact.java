28658e70cd37b6eef76f3a36974a3f97b85478d9
Ricardo <rgoncalo@campus.fct.unl.pt>
2022-09-17 12:32:12 +0100

    Removidas coisas desnecessárias que eu não tinha percebido bem o problema

--- src/contactBook/Contact.java
+++ src/contactBook/Contact.java
@@ -6,18 +6,12 @@ public class Contact {
     private String name;
     private int phone;
 
-    private HashMap<String, Integer> additionalPhones;
-
     private String email;
-
-    private HashMap<String, String> additionalEmails;
 
     public Contact(String name, int phone, String email) {
         this.name = name;
         this.phone = phone;
         this.email = email;
-        additionalEmails = new HashMap<String, String>();
-        additionalPhones = new HashMap<String, Integer>();
     }
 
     public String getName() {
@@ -35,14 +29,6 @@ public class Contact {
     public void setPhone(int phone) {
         this.phone = phone;
     }
-
-    public void addOtherEmail(String key, String email){ additionalEmails.put(key, email); }
-
-    public void addOtherNumber(String key, int number){ additionalPhones.put(key, number); }
-
-    public void remOtherEmail(String key){ additionalEmails.remove(key);}
-
-    public void remOtherNumber(String key) {additionalPhones.remove(key);}
 
     public void setEmail(String email) {
         this.email = email;
